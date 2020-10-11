853af96a109b727287bb380c1c7555fd22aac5a6
Liga <ligabaikova@gmail.com>
2020-10-11 18:58:53 +0100

    Implemented functionality for a user to be able to add a new task and then view it

--- app.py
+++ app.py
@@ -23,6 +23,19 @@ def get_tasks():
     # Then assign tasks to 'tasks' collection under task_manager database and find() will return everything that's in it (mongo=PyMongo(app))
     return render_template("tasks.html", tasks = mongo.db.tasks.find())
 
+# Add task on this route and use categories catallog to display categories 
+@app.route('/add_task')
+def add_task():
+    return render_template('addtasks.html', categories=mongo.db.categories.find())
+
+# When a task is submitted on the add_task page, it is posted to insert_task which inserts data in the tasks collection and converts data to a dictionary. Once done it redirects the user to the get_task page and newly added task is dipslayed
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    # new variable that equates the tasks collection
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
 if __name__ == '__main__':
     app.run(host = os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
