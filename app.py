b8ec9f539a5f580e48185bd71e01e076905e06d7
Liga <ligabaikova@gmail.com>
2020-10-14 19:38:19 +0100

    Add Session User Profile

--- app.py
+++ app.py
@@ -51,6 +51,7 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Succesful")
+        return redirect(url_for('profile', username = session["user"]))
     return render_template("register.html")
 
 
@@ -68,6 +69,7 @@ def login():
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
+                    return redirect(url_for('profile', username = session["user"]))
             else:
                 # Invalid password match
                 flash("Incorrect Username and/or Password")
@@ -79,6 +81,15 @@ def login():
             return redirect(url_for('login'))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # Grab the sessions user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username = username)
+
 
 # Add task on this route and use categories catallog to display categories 
 @app.route('/add_task')
